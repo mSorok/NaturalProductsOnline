0851cc792a90b7ab7343ec178f10f5101501f436
Maria Sorokina <maria.ssorokina@gmail.com>
2020-05-26 18:12:15 +0200

    updated About.js

--- src/main/js/components/About.js
+++ src/main/js/components/About.js
@@ -19,7 +19,7 @@ export default class About extends React.Component {
                     <p>Natural Products Online is an open source project for Natural Products (NPs) storage, search and analysis. The present version hosts COCONUT, the
                         COlleCtion of Open Natural ProdUcTs, one of the biggest and best annotated resources for NPs available free of charge and without any restriction.
                     </p>
-                    <p> https://github.com/mSorok/NaturalProductsOnline </p>
+                    <p> Please submit bug reports, feature requests and general issues through the issues tracker at <a href="https://github.com/mSorok/NaturalProductsOnline">GitHub</a>. </p>
                 </Row>
                 <br/>
                 <Row className="justify-content-center">
